350a712e58964507dd3f1961e3bf639ba2a5b675
黄斌杰 <akira.binjie@gmail.com>
2018-11-12 19:05:17 +0800

    init

--- src/useStore.js
+++ src/useStore.js
@@ -3,44 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var context_1 = require("./context");
 var shallowPartialEqual_1 = require("./shallowPartialEqual");
-var stateCache = new Map();
 function useStore(storeClass) {
     var system = react_1.useContext(context_1.RactorContext).system;
     var storeRef = system.get(storeClass);
     var store = storeRef ? storeRef.getInstance() : system.actorOf(new storeClass).getInstance();
-    // _store["__mountStatus__"] = _store["__mountStatus__"] || (storeRef ? "global" : "local")
-    // if (_store["__mountStatus__"] === "local") {
-    //   const cachedState = stateCache.get(storeClass)
-    //   if (cachedState) {
-    //     _store.state = cachedState
-    //   } else {
-    //     stateCache.set(storeClass, _store.state)
-    //   }
-    // }
-    var _a = react_1.useState(store), _store = _a[0], setStore = _a[1];
-    var _b = react_1.useState(_store.state), state = _b[0], setState = _b[1];
+    store["__mountStatus__"] = store["__mountStatus__"] || (storeRef ? "global" : "local");
+    var _a = react_1.useState(store.state), state = _a[0], setState = _a[1];
     react_1.useEffect(function () {
-    });
+        if (store["__mountStatus__"] === "local") {
+            store.context.stop();
+        }
+    }, []);
     react_1.useEffect(function () {
-        var subscription = _store.subscribe(function (nextState) {
+        var subscription = store.subscribe(function (nextState) {
             if (!shallowPartialEqual_1.default(nextState, state)) {
-                console.log(1111);
-                stateCache.set(storeClass, nextState);
                 setState(nextState);
             }
         });
-        return function () {
-            if (_store["__mountStatus__"] === "local") {
-                if (system.get(storeClass)) {
-                    console.log(222);
-                    stateCache.delete(storeClass);
-                    console.log(3333, _store.context.path);
-                    system.getRoot().getContext().stop(_store.context.self);
-                    console.log(system.getRoot().getContext().children);
-                }
-            }
-            subscription.unsubscribe();
-        };
+        return subscription.unsubscribe;
     });
     return [state, system.dispatch.bind(system)];
 }
--- src/useStore.ts
+++ src/useStore.ts
@@ -3,50 +3,27 @@ import { Store } from "ractor"
 import { RactorContext } from "./context";
 import shallowPartialEqual from "./shallowPartialEqual";
 
-const stateCache = new Map<new () => Store<any>, any>()
-
-export type StoreClass<T> = new (...args: any[]) => Store<T>
-
-export function useStore<T>(storeClass: StoreClass<T>): [T, (message: object) => void] {
+export function useStore<T>(storeClass: new (...args: any[]) => Store<T>): [T, (message: object) => void] {
   const { system } = useContext(RactorContext)
-  // _store["__mountStatus__"] = _store["__mountStatus__"] || (storeRef ? "global" : "local")
-  // if (_store["__mountStatus__"] === "local") {
-  //   const cachedState = stateCache.get(storeClass)
-  //   if (cachedState) {
-  //     _store.state = cachedState
-  //   } else {
-  //     stateCache.set(storeClass, _store.state)
-  //   }
-  // }
-  // const [_store, setStore] = useState(store)
-  const [state, setState] = useState(null)
+  const storeRef = system.get(storeClass)
+  const store: Store<T> & { [key: string]: any } = storeRef ? storeRef.getInstance() : system.actorOf(new storeClass).getInstance()
+  store["__mountStatus__"] = store["__mountStatus__"] || (storeRef ? "global" : "local")
 
-  if (state === null) {
-    const storeRef = system.get(storeClass)
-    const store = storeRef ? storeRef.getInstance() : system.actorOf(new storeClass).getInstance() as Store<T>
-    setState(store.state)
-  }
+  const [state, setState] = useState(store.state)
 
   useEffect(() => {
-    const subscription = _store.subscribe(nextState => {
+    if (store["__mountStatus__"] === "local") {
+      store.context.stop()
+    }
+  }, [])
+
+  useEffect(() => {
+    const subscription = store.subscribe(nextState => {
       if (!shallowPartialEqual(nextState, state)) {
-        console.log(1111)
-        stateCache.set(storeClass, nextState)
         setState(nextState)
       }
     })
-    return () => {
-      if (_store["__mountStatus__"] === "local") {
-        if (system.get(storeClass)) {
-          console.log(222)
-          stateCache.delete(storeClass)
-          console.log(3333, _store.context.path)
-          system.getRoot().getContext().stop(_store.context.self)
-          console.log(system.getRoot().getContext().children)
-        }
-      }
-      subscription.unsubscribe()
-    }
+    return subscription.unsubscribe
   })
   return [state, system.dispatch.bind(system)]
 }
